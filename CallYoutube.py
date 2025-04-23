5af4b5cb679c54015918d9572ffa24705f31b935
Ryan Keen <ryankeen19@gmail.com>
2025-04-22 23:27:36 -0700

    Create Youtube API call. Next is to connect the API call to the ProcessInput classO

--- CallYoutube.py
+++ CallYoutube.py
@@ -1,6 +1,52 @@
+import argparse
+from pprint import pprint
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
 import ProcessInput
+from youtube_credentials import DEVELOPER_KEY
+
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
 
 
 class CallYoutube:
+
+    YOUTUBE_URL_PREFIX = "https://www.youtube.com/watch?v="
+
     def __init__(self, search_dict: dict):
         self.search_dict = search_dict
+        self.artist = search_dict.get("artist", "")
+        self.songs = search_dict.get("songs", [])
+
+    def search_youtube(self, artist, song) -> list:
+        return_list = []
+
+        youtube = build(
+            YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY
+        )
+
+        # Call the search.list method to retrieve results matching the specified query term.
+        search_response = (
+            youtube.search().list(q=f"{artist} - {song}", part="snippet").execute()
+        )
+        response_items = search_response.get("items", [])
+
+        for item in response_items:
+            if item["id"]["kind"] == "youtube#video":
+                # Print the title and video ID of each search result
+                title = item["snippet"]["title"]
+                video_id = item["id"].get("videoId")
+                if video_id:
+                    print(f"Title: {title}, Video ID: {video_id}")
+                    url = f"{self.YOUTUBE_URL_PREFIX}{video_id}"
+                    return_list.append(url)
+                    break
+
+        return return_list
+
+
+test_case = CallYoutube({"artist": "Hozier", "songs": []})
+url = test_case.search_youtube("hozier", "jackie and wilson")
+print(url)
